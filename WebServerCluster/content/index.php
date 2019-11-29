d826473ebba075c9d4bdda735f8a2c0fbe40d70b
t0m-a <simon_thomas@outlook.fr>
2019-11-29 10:35:26 -0400

    Work in progress

---
+++ WebServerCluster/content/index.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+	<head>
+		<title>OK</title>
+	</head>
+	<body>
+		<h1>PHP OK</h1>
+		<p>Your PHP properties are shown below.</p>
+		<p>Do NOT do this in Production</p>
+		<p>For Development environment tests only.</p>
+		<?php
+		phpinfo();
+		?>
+	</body>
+</html>
