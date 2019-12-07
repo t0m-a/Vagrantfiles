7a2e056d853306369b87a0a357d03a08de6658b6
t0m-a <simon_thomas@outlook.fr>
2019-12-07 14:36:18 -0400

    boostrap installs nginx and golang

--- devBox_digitalOcean/bootstrap.sh
+++ devBox_digitalOcean/bootstrap.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
      apt-get update && apt-get upgrade -y
-	 apt-get install nginx -y
+	apt-get install nginx -y
+     cd ~
+     curl --silent -L https://dl.google.com/go/go1.13.5.linux-amd64.tar.gz && tar -xvf go1.13.5.linux-amd64.tar.gz && mv go /usr/local
+     printf '# Golang\nexport GOROOT=/usr/local/go\nexport GOPATH=$HOME/workspaces/go\nexport PATH=$GOPATH/bin:$GOROOT/bin:$PATH\n\n' >> .profile
      exit 0
